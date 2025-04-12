1a7b04aa5d6e7a24df244d11c719a6718d9a09a3
Eggexe <159236595+Eggexe@users.noreply.github.com>
2025-04-12 19:40:46 +0100

    adds a draw rocket func may change to rocket class later

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 from physics import PhysicsLibrary
 
-phys = PhysicsLibrary(0,0,0,0,0)
-
 #make crash variable to quit game if game crashes
 game_end = False
 (width, height) = (1800, 1000)
@@ -12,10 +10,19 @@ screen = pygame.display.set_mode((width,height))
 # put screen mods INSIDE this comment
 background_colour = (255, 255, 255)
 screen.fill(background_colour)
+clock = pygame.time.Clock() # sets FPS to 60, the "t" of suvat from phys obj
+phys = PhysicsLibrary(0,0,0,0,clock)
 # place screen mods above here
+
+rocket_1 = pygame.Rect(200, 100, 150, 100) #
+
 pygame.display.flip()
 
 while not game_end:
+    clock.tick(60)
+
+    pygame.draw.rect(screen, (200, 200, 200), rocket_1)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_end = True
+            pygame.quit()
