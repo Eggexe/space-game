d2d4e71c4d149fd4e4a7775e4764815208ec948d
Eggexe <159236595+Eggexe@users.noreply.github.com>
2025-04-16 14:25:50 +0100

    adds rocket.py class for rockets removes pygame.draw.rect func from main, soon to be moved into rocket class as a render function

--- main.py
+++ main.py
@@ -14,14 +14,10 @@ clock = pygame.time.Clock() # sets FPS to 60, the "t" of suvat from phys obj
 phys = PhysicsLibrary(0,0,0,0,clock)
 # place screen mods above here
 
-rocket_1 = pygame.Rect(200, 100, 150, 100) #
-
 pygame.display.flip()
 
 while not game_end:
     clock.tick(60)
-
-    pygame.draw.rect(screen, (200, 200, 200), rocket_1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
