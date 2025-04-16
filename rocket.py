a778918f211dba9950bc7feb86fb9bd2685bbef3
Eggexe <159236595+Eggexe@users.noreply.github.com>
2025-04-16 14:45:47 +0100

    rocket rendering adds rocket draw func in rocket.py modified main.py to accomodate this changed some names of vars to reduce confusion

--- rocket.py
+++ rocket.py
@@ -1,6 +1,15 @@
 import pygame
-from main import screen
 
-class Rocket():
-    def __init__(rocketName):
-        self.name = rocketName
+class RocketBase():
+    def __init__(self, x, y):
+        self.pos_x = x
+        self.pos_y = y
+
+        # add images for rockets here
+        self.image = pygame.image.load("rocket_1.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 100))
+        # end of images
+        self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
