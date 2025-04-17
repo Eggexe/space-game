9fcf26438fe22e54c132e30ba38f049c64c77b63
Eggexe <159236595+Eggexe@users.noreply.github.com>
2025-04-17 10:01:48 +0100

    BIG CHANGE TO PHYSICS AND ROCKET LIB adds more looser code to physics lib - no need to pass anything in anymore - adds dict to return all force vars to us - added thrust dict for all the fuel + their fuel amounts - added ratio balancing which gets returned on get_thrust
    adds more stuff to rocket.py
    - adds more internal vars
    - rewrote some ones to pass in
    -- eventually will be user selected
    - adds a clever update func that too wayy too long to make :(
    - kept draw func the same
    
    main.py
    - just rewrote it to have functionality for the updated libraries and 
    cleaned up the comments a bit
    
    hours spent: 8 :(

--- main.py
+++ main.py
@@ -2,29 +2,43 @@ import pygame
 from physics import PhysicsLibrary
 from rocket import RocketBase
 
-#make crash variable to quit game if game crashes
+# whoopsie variable in case something breaks and i need to crash early to close
 game_end = False
-(width, height) = (1800, 1000)
-screen = pygame.display.set_mode((width,height))
 
-#flip must happen before screen effects take place
-# put screen mods INSIDE this comment
+# window config
+(width, height) = (1800, 1000)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Rocket Simulation")
+
+# clock
+clock = pygame.time.Clock()
+
+# instantiate physics lib
+phys = PhysicsLibrary()
+rocket_1 = RocketBase(0,0,0,0)
+
+# screen colour, can change later to a blue for sky
 background_colour = (255, 255, 255)
 screen.fill(background_colour)
-clock = pygame.time.Clock() # sets FPS to 60, the "t" of suvat from phys obj
-phys = PhysicsLibrary(0,0,0,0,clock)
-rocket_1 = RocketBase(900, 500)
-# place screen mods above here
-
 pygame.display.flip()
 
+# game loop
 while not game_end:
-    clock.tick(60)
+    dt = clock.tick(60) / 1000  # delta time in seconds
 
+    # event pygame handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            game_end = True
+
+    # make screen look nice
+    screen.fill(background_colour)
+
+    # update and draw rocket position with dt and physics lib
+    rocket_1.update(dt, phys)
     rocket_1.draw(screen)
+
+    # update display
     pygame.display.flip()
 
 pygame.quit()
--- rocket.py
+++ rocket.py
@@ -1,15 +1,41 @@
 import pygame
 
 class RocketBase():
-    def __init__(self, x, y):
-        self.pos_x = x
-        self.pos_y = y
+    def __init__(self, rocket_type, fuel_type, oxidiser, fuel_oxi_ratio):
+        # thruster variables
+        self.fuel_mass = 500
+        self.fuel_type = fuel_type
+        self.oxidiser = oxidiser
+        self.fuel_oxi_ratio = fuel_oxi_ratio
+        self.thrust = 0
+        self.mass = 1000
 
-        # add images for rockets here
+        # movement variables
+        self.pos_x = 900 #change to looser opt eventually
+        self.pos_y = 500 #change to looser opt eventually
+        self.velocity = 0
+        self.acceleration = 0
+
+        #images - preloads image from images dir and adds it to transform var and renders
         self.image = pygame.image.load("rocket_1.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (50, 100))
-        # end of images
+        self.image = pygame.transform.scale(self.image, (100, 150))
         self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
 
-    def draw(self, surface):
+    def update(self, dt, physics_engine): # global physics updater v2
+        # hours spent on this: idk like 2.5 (way too long imo)
+        forces = physics_engine.calculate_forces(self) # calls physics engine code to calculate forces
+        # okay physics nerds will remember F=ma (big guy's second law)
+        # this then means that F=ma can be rewritten as a = F/m
+        # acceleration is then used to well, accelerate
+        # the forces['net'] is just the total force added, hence the term 'net'
+        self.acceleration = forces["net"] / self.mass
+
+        # this is just velocity, not much to explain apart from dt meaning deltatime
+        # delta timing is a good thing to use to keep framerates the same
+        # this is why it's forced to be 60 in main.py as 60 fps is a standard
+        self.velocity += self.acceleration * dt
+        self.pos_y -= self.velocity * dt # since pygame has y being at the top of screen, we subtract to go down
+        self.rect.center = (self.pos_x, self.pos_y) # this just draws the image, not special
+
+    def draw(self, surface): # draw function to update position of rocket
         surface.blit(self.image, self.rect)
